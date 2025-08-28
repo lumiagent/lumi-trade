fd06532a6a670ea6eb11fc3a76a56fdead1a4513
LUMI AGENT <lumiagentsol@gmail.com>
2025-08-28 12:16:19 -0700

    Update and rename main.tsx to main.old2.tsx

--- src/main.old2.tsx
+++ src/main.old2.tsx
@@ -9,7 +9,7 @@ if (typeof window !== 'undefined') {
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './style.css';
+import './.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
